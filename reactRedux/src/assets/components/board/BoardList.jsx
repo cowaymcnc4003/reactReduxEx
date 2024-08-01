addecf67404e77a822fb215bdf1607ff605997dc
mhlee <mcnccoway4003@gmail.com>
2024-08-01 14:08:25 +0900

    design

---
+++ reactRedux/src/assets/components/board/BoardList.jsx
@@ -0,0 +1,42 @@
+// import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+// import BoardItem from "./BoardItem";
+// import { selectBoard, deleteBoard } from "../../redux/slices/boardSlice";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const BoardList = (props) => {
+  // const boards = useSelector((state) => state.board.boards);
+  // const selectedBoardId = useSelector((state) => state.board.selectedBoardId);
+
+  // const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      {/* {boards.map((board, index) => {
+        const isSelected = board.id === selectedBoardId;
+        console.log(board.id);
+        console.log(isSelected);
+        return (
+          <BoardItem
+            key={index}
+            board={board}
+            isSelected={isSelected}
+            onSelect={() => {
+              dispatch(selectBoard(board.id));
+            }}
+            onDelete={() => {
+              dispatch(deleteBoard(board.id));
+            }}
+          />
+        )
+      })} */}
+    </Wrapper>
+  );
+};
+
+export default BoardList;
---
+++ reactRedux/src/assets/components/board/BoardMenu.jsx
@@ -0,0 +1,47 @@
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+import styled from "styled-components";
+// import { createBoard } from "../../redux/slices/boardSlice";
+
+const Wrapper = styled.div`
+  padding: 16px;
+`;
+
+const SyledInput = styled.input`
+  padding: 16px;
+`;
+
+const StyledButton = styled.button`
+  padding: 16px;
+`;
+
+const BoardMenu = () => {
+  // const dispatch = useDispatch();
+
+  // const [newBoardName, setNewBoardName] = useState('');
+  return (
+    <Wrapper>
+      <SyledInput
+      // value={newBoardName}
+      // onChange={(e) => {
+      //   setNewBoardName(e.target.value);
+      // }}
+      />
+      <StyledButton
+      // onClick={() => {
+      //   if (newBoardName === '') {
+      //     alert('추가할 보드 이름을 입력해주세요');
+      //     return;
+      //   }
+
+      //   dispatch(createBoard(newBoardName));
+      //   setNewBoardName('');
+      // }}
+      >
+        보드 추가하기
+      </StyledButton>
+    </Wrapper>
+  );
+};
+
+export default BoardMenu;
