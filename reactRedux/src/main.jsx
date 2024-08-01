322a5a97f510b29a33f58054d789927a3b256761
mhlee <mcnccoway4003@gmail.com>
2024-08-01 14:37:32 +0900

    redux 연동

--- reactRedux/src/main.jsx
+++ reactRedux/src/main.jsx
@@ -1,7 +1,14 @@
 import App from './assets/components/App'
 import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux';
+import store from './assets/redux/store';
+
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
